711c83cec738f300b47d9920a5515101cc2a1d12
Christian Vohwinkel <christian.vohwinkel@flexlog.de>
2026-07-24 15:56:56 +0200

    remove some comments

--- src/Application/main.cpp
+++ src/Application/main.cpp
@@ -4,13 +4,11 @@
 #include "GLFW/glfw3.h"  // For window creation
 
 int main() {
-    // 1. Initialize GLFW (required for Vulkan surface)
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
-    // 3. Initialize the Renderer/Vulkan Device
     Renderer::Device device;
     device.initialize();
 
